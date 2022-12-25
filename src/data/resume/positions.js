dd376d6fea174f0853c6b2c6705fd4175ee853f5
Ishwar Sawale <ishwar.code@gmail.com>
2022-12-25 13:20:34 +0100

    feat(work): update details for recom
    Reviewed-by: Ishwar Sawale
    Refs master

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -6,6 +6,7 @@ const positions = [
     daterange: 'Sept 2021 - Present',
     points: [
     ],
+      'Built recommendation system for the Dazn Platform.',
   },
   {
     company: 'Indegene Inc',
@@ -14,38 +15,24 @@ const positions = [
     daterange: 'May 2020 - Aug 2021',
     points: [
       'Automated Ad Creative Generation & Recommendation product for Life Science companies from historical data.',
-      'Developed Object detection, Text extraction & Classification models to categorize assets using Tensorflow & Deep learning algorithms.',
-      'Built a creative layout generation model using Generative Adversarial Network resulting in reducing content creation time by 20%.',
-      'Created HTML template from GAN generated layout which creates final Ad creative using assets repository.',
     ],
   },
   {
-    company: 'Mindstix Labs (Client VMware IT)',
+    company: 'Mindstix Labs',
     position: 'Senior Data Scientist',
     link: 'https://www.mindstix.com/',
     daterange: 'Dec 2018 - April 2020',
     points: [
-      'Created a text-independent speaker recognition system for VMware',
-      'Implemented new architecture based on ResNet with 34 layers & 3 million parameters using Tensorflow.',
-      'Trained on 10GB of speaker data for 8K Classes using Adam optimizer with additive margin softmax loss.',
-      'Created a digital chatbot assistant for VMware to help with employee issue resolution',
-      'Developed Natural Language Understanding framework for entity recognition, intent classification & dialogue management using Tensorflow, Rasa & Spacy.',
-      'Modified RASA framework to remember slot values based on dialogue context, added support for custom Entity recognition pipeline, added support for Slack, Teams channels.',
-      'Added a new data pipeline for RASA that accepts data in different formats.',
-      'Created Question Answering system using Tensorflow by transfer learning Universal sentence encoder on customer dataset of 5k Q&A pairs.',
-      'Built & exposed prediction, train services as REST APIs using Django & distributed using Docker containers, deployed on the AWS stack.',
+      'Created a digital chatbot assistant for client to help with employee issue resolution',
     ],
   },
   {
-    company: 'Mindstix Labs (Client Estée Lauder)',
+    company: 'Mindstix Labs',
     position: 'Data Scientist',
     link: 'https://www.mindstix.com/',
     daterange: 'February 2018 - December 2018',
     points: [
       'Designed & developed a Computer Vision algorithm to remove illumination from images using Deep learning.',
-      'Created novel Fully Convolutional Network using AlexNet architecture with angular error loss function, network outperforms the previous state-of-the-art while achieving 120 times greater efficiency.',
-      'Implemented Neural network using Tensorflow, Numpy, OpenCV, Python.',
-      'Built & exposed REST APIs backend service for predictions using Django for mobile applications used by 10K users.',
     ],
   },
   {
@@ -55,9 +42,6 @@ const positions = [
     daterange: 'February 2016 - January 2018',
     points: [
       'Created Automatic License Plate Recognition System using Deep Learning algorithms for Indian vehicles.',
-      'Developed CNN Networks for Vehicle detection, License Plate detection, Character segmentation, Alphabet recognition & Digit recognition using Python, Tensorflow, YOLO.',
-      'Created Indian License Plate dataset with 4000 annotated images collected from real-world samples.',
-      'Built a GUI interface using PyQT that connects to the camera module & stores recognized vehicle information in the MySQL database.',
     ],
   },
 ];

27ef3b41530b706595abcf88e67d1fb19c79ae21
Ishwar Sawale <ishwar.code@gmail.com>
2023-02-17 21:20:35 +0100

    added routes

--- src/data/routes.js
+++ src/data/routes.js
@@ -2,7 +2,30 @@ const routes = [
   {
     index: true,
     label: 'Ishwar Sawale',
+    path: '/',
+  },
+  {
+    label: 'About',
     path: '/about',
+  },
+  /*
+  {
+    label: 'Résumé',
+    path: '/resume',
+  },
+    {
+    label: 'Projects',
+    path: '/projects',
+  },
+  {
+    label: 'Stats',
+    path: '/stats',
+  },
+
+  */
+  {
+    label: 'Certifications',
+    path: '/mooc',
   },
   {
     label: 'Contact',
